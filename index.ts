6f364eb3722300a1ce271c535cb5e684d846ac27
Markie Sprout <79816739+vtaoki123@users.noreply.github.com>
2021-03-12 19:02:08 -0500

    Summary

--- index.ts
+++
@@ -1,8 +0,0 @@
-let map: google.maps.Map;
-
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
